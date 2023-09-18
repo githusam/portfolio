66326500af752a3b3c50915df90b43a6d825145f
Zeytoun1 <husamkassab02@gmail.com>
2023-09-18 07:30:18 -0400

    adding sections and its details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method="POST" action="https://getform.io/f/991fa7fe-87e0-4362-8c5b-753e0119a229" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#0097b2] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - husamkassab02@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+            <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='email' />
-            <textarea name="message" id="" cols="30" rows="10"></textarea>        
+            <textarea className='bg-[#ccd6f6] p-2' name="message" id="" cols="30" rows="10" placeholder = 'Message'></textarea>        
+            <button className='text-white border-2 hover:bg-[#0097b2] hover:border-[#0097b2] px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>       
         </form>
     </div>
   )
